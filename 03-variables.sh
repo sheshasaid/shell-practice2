c0dea2a4195b76608daa1116145d3bd8723e2d08
Sheshasai Dyavanapally <Sheshasai>
2026-01-15 01:26:28 +0530

    conversations

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: Hi $PERSON2, How are you.."
+echo "$PERSON2:: Hi $PERSON1, I am good thanks."
+echo "$PERSON1:: Where are you!!"
+echo "$PERSON2 :: I am in hyderabad."
