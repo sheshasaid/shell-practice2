a93e733b21cf322e2e465435ed8ee07449e4a379
Sheshasai Dyavanapally <Sheshasai>
2026-01-15 01:41:14 +0530

    conversations

---
+++ 04-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Modi:: Hi Sheshu, How are you..!!"
+echo "Sheshu:: Hi Sir, I am good, how are you..!!"
+echo "Modi:: I am well, Where are you..!!"
+echo "Sheshu:: I am in Karimnagar."
