0823638a72b3c7875275d0dd1bf6def45b24aaae
Sheshasai Dyavanapally <Sheshasai>
2026-01-15 01:43:25 +0530

    conversations

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,7 +4,7 @@
 $PERSON1=$1
 $PERSON2=$2
 
-echo "Modi:: Hi Sheshu, How are you..!!"
-echo "Sheshu:: Hi Sir, I am good, how are you..!!"
-echo "Modi:: I am well, Where are you..!!"
-echo "Sheshu:: I am in Karimnagar."
+echo "$PERSON1:: Hi $PERSON2, How are you..!!"
+echo "$PERSON2:: Hi Sir, I am good, how are you..!!"
+echo "$PERSON1:: I am well, Where are you..!!"
+echo "$PERSON2:: I am in Karimnagar."
