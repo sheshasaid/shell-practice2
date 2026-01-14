2b82f1aa209bd7b3a548fbb4407f058f2996ecad
Sheshasai Dyavanapally <Sheshasai>
2026-01-15 01:18:18 +0530

    conversations

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Ramesh:: Hi Suresh, How are you..
-echo "Suresh:: Hi Ramesh, I am good thanks.
-echo "Ramesh:: Where are you!!
-echo "Suresh :: I am in hyderabad.
+echo "Ramesh:: Hi Suresh, How are you.."
+echo "Suresh:: Hi Ramesh, I am good thanks."
+echo "Ramesh:: Where are you!!"
+echo "Suresh :: I am in hyderabad."
