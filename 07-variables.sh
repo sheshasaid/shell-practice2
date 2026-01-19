d082bd29006082e06a934d3d6a7b5f623cfbf2b6
Sheshasai Dyavanapally <Sheshasai>
2026-01-19 16:03:00 +0530

    Variables

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-DATE=$(date)
+#DATE=$(date)
+
+
+date=$(date %F)
+
+START_TIME=$(date +%s)
+
+sleep 10 &
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$($START_TIME-$END_TIME)
+
+echo "Script executed in: $TOTAL_TIME Seconds"
