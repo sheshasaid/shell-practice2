a801f4c5a4b59f2bf84b724893302cabed5cf1ee
Sheshasai Dyavanapally <Sheshasai>
2026-01-19 16:13:53 +0530

    Variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -5,7 +5,10 @@ echo "All variables passed to the script: $*"
 
 echo "Script name is: $0"
 
-echo "Present working directory: $pwd"
-echo "Who is the user: $whoami"
+echo "Present working directory: $PWD"
+echo "Who is the user: $USER"
+echo "Home directory of the user: $HOME"
+echo "PID of the script: $$"
+
 
 
